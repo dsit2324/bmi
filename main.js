f15ba7bec3bf8e0521d0164f47d9788c9e525a8a
dsit2324 <148313992+dsit2324@users.noreply.github.com>
2024-11-20 10:35:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-const weight = document.getElementById("weight");
-const height = document.getElementById("height");
-const output = document.getElementById("vystup");
-const btnCalc = document.querySelector("[type=button]");
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const output = document.getElementById('vystup');
+const btnCalc = document.querySelector('[type=button]');
 
-btnCalc.addEventListener('click',  function() {
-    weight.value++;
-    console.log(weight.value);
+let BMI = {
+    // atributy objektu 
+    gender: null,
+    age: 18,
+    weight: 70,
+    height: 170,
+    // metody objektu 
+    cmToMetres: function() {
+        return this.height / 100;
+    },
+    calcBMI: function() {
+        return (this.weight / (this.cmToMetres() ** 2)).toFixed(2);
+    },
+    state: function() {
+        let bmi = this.calcBMI;
+        if (bmi < 18.5) return 'podváha';
+        if (bmi >= 18.5 && bmi < 25) return 'normální váha';
+        if (bmi >= 25 && bmi < 30) return 'nadváha';
+        return 'obezita';
+    }
+}
+
+btnCalc.addEventListener('click', function() {
+    // weight.value++;
+    BMI.weight = parseInt(weight.value); //parseInt je pretypovani 
+    BMI.height = parseInt(height.value);
+    console.log(BMI.calcBMI());
+    output.innerHTML = `Tvoje BMI je ${BMI.calcBMI()}, což je  ${BMI.state()}`
+
 });
+
